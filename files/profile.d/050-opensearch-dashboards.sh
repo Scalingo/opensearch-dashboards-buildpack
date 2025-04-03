30f03c669326314177c2cf9f2be6ea84f53e49b8
François KUBLER <francois+github@kubler.org>
2025-04-03 09:38:28 +0200

    fix: generate config at runtime
    Allows OpenSearch Dashboards to be rebooted when an environment variable is updated (no deployment required for it to be rebooted with the new values).

--- files/profile.d/050-opensearch-dashboards.sh
+++ files/profile.d/050-opensearch-dashboards.sh
@@ -2,3 +2,15 @@
 
 PATH="${PATH}:${HOME}/bin"
 export PATH
+
+# Populates OpenSearch Dashboards config file:
+
+cat <<- EOF >> "${HOME}/config/opensearch_dashboards.yml"
+server:
+  host: 0.0.0.0
+  port: ${PORT}
+
+opensearch:
+  hosts:
+    - "${OPENSEARCH_URL}"
+EOF
